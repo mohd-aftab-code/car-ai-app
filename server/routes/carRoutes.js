279892a374048ed083754cc61927f2af3c989611
mohd-aftab-code <aftabansari92751@gmail.com>
2026-04-27 12:49:12 +0530

    docs: remove obsolete documentation files

--- server/routes/carRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCarRecommendations } = require('../controllers/carController');
-
-router.post('/recommend', getCarRecommendations);
-
-module.exports = router;
